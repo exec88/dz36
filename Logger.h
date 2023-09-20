684213245ae9cc3acc3bf69f8f134dd25808e3d3
exec88 <117723681+exec88@users.noreply.github.com>
2023-09-20 20:33:15 +0400

    Add files via upload

---
+++ Logger.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <mutex>
+
+class Logger
+{
+private:
+	std::fstream _fileStream;
+    std::mutex _mutex;
+
+public:
+	Logger(std::string filename);
+	~Logger();
+
+	bool writoToFile(std::string text);
+	std::string readFromFileOneLine();
+};
